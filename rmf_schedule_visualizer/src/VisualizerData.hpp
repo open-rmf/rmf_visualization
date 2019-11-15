3fca1d10a2b7f14a4f83dcc7077a73d16cbd6630
Yadunund <yadunund@openrobotics.org>
2019-11-15 18:30:26 +0800

    added source and header files

---
+++ rmf_schedule_visualizer/src/VisualizerData.hpp
@@ -0,0 +1,39 @@
+/*
+ * Copyright (C) 2019 Open Source Robotics Foundation
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+*/
+
+
+#ifndef RMF_SCHEDULE_VISUALIZER__SRC__VISUALIZERDATA.HPP
+#define RMF_SCHEDULE_VISUALIZER__SRC__VISUALIZERDATA.HPP
+
+#include <rmf_traffic_ros2/schedule/MirrorManager.hpp>
+#include <rmf_traffic/Trajectory.hpp>
+#include <rclcpp/node.hpp>
+
+namespace rmf_schedule_visualizer {
+
+class VisualizerNode : public rclcpp::Node
+{
+public:
+  
+private:
+      
+};
+
+
+} //namespace rmf_schedule_visualizer
+
+#endif // RMF_SCHEDULE_VISUALIZER__SRC__VISUALIZERDATA.HPP
