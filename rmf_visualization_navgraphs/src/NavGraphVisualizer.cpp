0ac3ab71f07d95bdfbb3ac64d66a910285e8263e
Yadunund <yadunund@gmail.com>
2022-06-09 17:48:29 +0800

    Skeleton for navgraph visualization
    Signed-off-by: Yadunund <yadunund@gmail.com>

---
+++ rmf_visualization_navgraphs/src/NavGraphVisualizer.cpp
@@ -0,0 +1,82 @@
+/*
+ * Copyright (C) 2022 Open Source Robotics Foundation
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+*/
+
+
+#include "NavGraphVisualizer.hpp"
+
+
+#include <rclcpp_components/register_node_macro.hpp>
+
+//==============================================================================
+NavGraphVisualizer::NavGraphVisualizer(const rclcpp::NodeOptions& options)
+: Node("navgraph_visualizer", options)
+{
+  _data = std::make_shared<Data>();
+
+  _data->current_level = this->declare_parameter("initial_map_nam", "L1");
+  RCLCPP_INFO(
+    this->get_logger(),
+    "Setting parameter initial_map_name to %s", _data->current_level.c_str());
+
+  _data->param_sub = this->create_subscription<RvizParam>(
+    "rmf_visualization/parameters",
+    rclcpp::SystemDefaultsQoS(),
+    [data = _data, n = weak_from_this()](std::shared_ptr<const RvizParam> msg)
+    {
+      if (!msg->map_name.empty())
+        data->current_level = msg->map_name;
+
+      if (auto node = n.lock())
+      {
+        RCLCPP_INFO(
+          node->get_logger(),
+          "Publishing navgraphs on level %s", data->current_level.c_str());
+      }
+
+      data->publish_navgraphs();
+    });
+
+  RCLCPP_INFO(
+    this->get_logger(),
+    "NavGraph visualizer is running...");
+}
+
+//==============================================================================
+void NavGraphVisualizer::Data::param_cb(std::shared_ptr<const RvizParam> msg)
+{
+
+}
+
+//==============================================================================
+void NavGraphVisualizer::Data::graph_cb(std::shared_ptr<const NavGraph> msg)
+{
+
+}
+
+//==============================================================================
+void NavGraphVisualizer::Data::lane_states_cb(std::shared_ptr<const LaneStates> msg)
+{
+
+}
+
+//==============================================================================
+void NavGraphVisualizer::Data::publish_navgraphs()
+{
+
+}
+
+RCLCPP_COMPONENTS_REGISTER_NODE(NavGraphVisualizer)
---
+++ rmf_visualization_navgraphs/src/NavGraphVisualizer.hpp
@@ -0,0 +1,65 @@
+/*
+ * Copyright (C) 2022 Open Source Robotics Foundation
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+*/
+
+#ifndef SRC__NAVGRAPHVISUALIZER_HPP
+#define SRC__NAVGRAPHVISUALIZER_HPP
+
+#include <rclcpp/rclcpp.hpp>
+
+#include <rmf_building_map_msgs/msg/graph.hpp>
+#include <rmf_fleet_msgs/msg/lane_states.hpp>
+
+#include <std_msgs/msg/color_rgba.hpp>
+#include <visualization_msgs/msg/marker_array.hpp>
+#include <visualization_msgs/msg/marker.hpp>
+
+#include <rmf_visualization_msgs/msg/rviz_param.hpp>
+
+
+//==============================================================================
+class NavGraphVisualizer : public rclcpp::Node
+{
+public:
+  using NavGraph = rmf_building_map_msgs::msg::Graph;
+  using LaneStates = rmf_fleet_msgs::msg::LaneStates;
+  using RvizParam = rmf_visualization_msgs::msg::RvizParam;
+
+  /// Constructor
+  NavGraphVisualizer(
+    const rclcpp::NodeOptions& options  = rclcpp::NodeOptions());
+
+private:
+  struct Data
+  {
+    void param_cb(std::shared_ptr<const RvizParam> msg);
+    void graph_cb(std::shared_ptr<const NavGraph> msg);
+    void lane_states_cb(std::shared_ptr<const LaneStates> msg);
+    void publish_navgraphs();
+
+    rclcpp::Subscription<RvizParam>::SharedPtr param_sub;
+    rclcpp::Subscription<NavGraph>::SharedPtr navgraph_sub;
+    rclcpp::Subscription<LaneStates>::SharedPtr lane_states_sub;
+
+    std::string current_level;
+  };
+
+  std::shared_ptr<Data> _data;
+
+};
+
+
+#endif // SRC__NAVGRAPHVISUALIZER_HPP
