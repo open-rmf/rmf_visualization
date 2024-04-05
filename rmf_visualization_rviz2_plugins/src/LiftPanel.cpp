1f331cd7e9c3e92fddc1a6bb7fb7ae5126091007
Luca Della Vedova <lucadv@intrinsic.ai>
2024-04-05 16:50:34 +0800

    Fix lift request QoS to be transient local
    Signed-off-by: Luca Della Vedova <lucadv@intrinsic.ai>

--- rmf_visualization_rviz2_plugins/src/LiftPanel.cpp
+++ rmf_visualization_rviz2_plugins/src/LiftPanel.cpp
@@ -32,6 +32,8 @@ LiftPanel::LiftPanel(QWidget* parent)
 {
   const auto default_qos =
     rclcpp::SystemDefaultsQoS().durability_volatile().keep_last(100).reliable();
+  const auto transient_qos =
+    rclcpp::SystemDefaultsQoS().transient_local().keep_last(100).reliable();
   _node = std::make_shared<rclcpp::Node>(_session_id + "_node");
   _lift_state_sub = _node->create_subscription<LiftState>(
     LiftStateTopicName, default_qos, [&](LiftState::UniquePtr msg)
@@ -39,9 +41,9 @@ LiftPanel::LiftPanel(QWidget* parent)
       lift_state_callback(std::move(msg));
     });
   _lift_request_pub = _node->create_publisher<LiftRequest>(
-    LiftRequestTopicName, rclcpp::QoS(10));
+    LiftRequestTopicName, transient_qos);
   _adapter_lift_request_pub = _node->create_publisher<LiftRequest>(
-    AdapterLiftRequestTopicName, rclcpp::QoS(10));
+    AdapterLiftRequestTopicName, transient_qos);
 
   create_layout();
   create_connections();
