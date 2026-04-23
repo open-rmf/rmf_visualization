d12d59688b98fe8e5c9cafe7227b453cd82f7c82
Luca Della Vedova <lucadv@intrinsic.ai>
2026-04-23 14:26:04 +0800

    Add static cast for buffer data
    Signed-off-by: Luca Della Vedova <lucadv@intrinsic.ai>

--- rmf_visualization_floorplans/src/FloorplanVisualizer.cpp
+++ rmf_visualization_floorplans/src/FloorplanVisualizer.cpp
@@ -74,7 +74,8 @@ FloorplanVisualizer::FloorplanVisualizer(const rclcpp::NodeOptions& options)
           continue;
 
         cv::Mat cv_img = cv::imdecode(
-          cv::Mat(level.images[0].data), cv::IMREAD_GRAYSCALE);
+          cv::Mat(static_cast<std::vector<uint8_t>>(level.images[0].data)),
+          cv::IMREAD_GRAYSCALE);
         auto it = level.images.begin();
         ++it;
         // We blend all the other images into the first image
@@ -84,7 +85,8 @@ FloorplanVisualizer::FloorplanVisualizer(const rclcpp::NodeOptions& options)
         for (; it != level.images.end(); ++it)
         {
           cv::Mat next_img = cv::imdecode(
-            cv::Mat(it->data), cv::IMREAD_GRAYSCALE);
+            cv::Mat(static_cast<std::vector<uint8_t>>(it->data)),
+            cv::IMREAD_GRAYSCALE);
           cv::addWeighted(cv_img, 0.7, next_img, 0.3, 0.0, cv_img);
         }
         const auto& image = level.images[0];
